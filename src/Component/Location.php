de177ceda9fe34790fcc9051babf35b8983c9a57
Kristers Andersons <kandersons@anytrades.co.uk>
2024-05-13 09:43:58 +0300

    Added Location component

---
+++ src/Component/Location.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace NotificationChannels\WhatsApp\Component;
+
+class Location extends Component
+{
+    protected string $name;
+    protected string $address;
+    protected float $latitude;
+    protected float $longitude;
+
+    public function __construct(string $name, string $address, float $latitude, float $longitude)
+    {
+        $this->name = $name;
+        $this->address = $address;
+        $this->latitude = $latitude;
+        $this->longitude = $longitude;
+    }
+
+    // TODO: PHP 8
+    // public function __construct(
+    //     protected string $name,
+    //     protected string $address,
+    //     protected float $latitude,
+    //     protected float $longitude
+    // ) {}
+
+    public function toArray(): array
+    {
+        return [
+            "type" => "location",
+            "location"=> [
+                "latitude" => (string)$this->latitude,
+                "longitude" => (string)$this->longitude,
+                "name" => $this->name,
+                "address" => $this->address,
+            ],
+        ];
+    }
+}
